a356243306caf1c525833bbe2294edb4b4c6b11a
imriiama <146062070+imriiama@users.noreply.github.com>
2025-01-21 17:36:20 +0100

    rendu

---
+++ A2-TU-Challenge-Unity-main/Assets/Tests/TU Challenge/MyStringImplementation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TU_Challenge
+{
+    public class MyStringImplementation
+    {
+        internal static bool IsNullEmptyOrWhiteSpace(string input)
+        {
+            throw new NotImplementedException();
+        }
+        internal static string MixString(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+        internal static string ToLowerCase(string a)
+        {
+            throw new NotImplementedException();
+        }
+        internal static string Voyelles(string a)
+        {
+            throw new NotImplementedException();
+        }
+        public static string Reverse(string s)
+        {
+            throw new NotImplementedException();
+        }
+        internal static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+        internal static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+        internal static string ToCesarCode(string input, int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
